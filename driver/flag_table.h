900bdf505793a2d3a55625d49db11bb093cd9388
Emmett-Kopczynski <emmettkopo@gmail.com>
2026-07-26 14:08:33 -0700

    finished documenting the flag_table.h file

--- driver/flag_table.h
+++ driver/flag_table.h
@@ -3,15 +3,10 @@
 /* flag_table.h :: The header file for the FlagLookupTable data structure, the Flags enum, and the associated
  * utility functions
  *
- * To Do : 
+ * To Do : NOTHING 
  *
  *      To Document :
- *          - FlagLookupTable
- *          - contains
- *          - puts
- *          - FlagLookupTable_construct_new
- *          - FlagLookupTable_contains
- *          - FlagLookupTable_put
+ *
  */
 
 /* homemade inclusions */
@@ -34,33 +29,52 @@ typedef enum {
 } Flags;
 
 
-/* FlagLookupTable :  TODO document
+/* FlagLookupTable :  A lookup table for the command line flags
+ *
+ * Variables :
+ *      - flaglist : type boolean [] : a list of booleans indicating whether a certain 
+ *      flag has been turned on or off
+ *
+ * Function Pointers : 
+ *      - contains : returns boolean : returns True if the given flag is marked as on in the table, and False if it is not
+ *      - put : returns boolean : marks the given flag as on in the table, if it was already on, returns False, otherwise returns True
  *
  */
 typedef struct flaglookuptable {
     
     boolean flaglist[FLAGNUM]; 
 
-    /* TODO document */
     boolean (*contains)(struct flaglookuptable *, Flags);
 
-    /* TODO document */
     boolean (*put)(struct flaglookuptable *, Flags);
     
 } FlagLookupTable;
 
-/* TODO document
+
+/* FlagLookupTable_construct_new :  TODO document
  *
  */
 void FlagLookupTable_construct_new(FlagLookupTable *flagtable);
 
-/*  TODO document
+/* FlagLookupTable_contains :  TODO document
  *
+ * Arguments : 
+ *      - flagtable : type FlagLookupTable * : the flagtable we are searching in
+ *      - flag : type Flags : the flag we are searching for in the table
+ *
+ * Returns : 
+ *      - True if it was in the table, False if it was not
  */
 boolean FlagLookupTable_contains(FlagLookupTable *flagtable, Flags flag);
 
-/* TODO document
+/* FlagLookupTable_put : puts the given flag in the table
  *
+ * Arguments : 
+ *      - flagtable : type FlagLookupTable * : the flag lookup table we are putting a flag in
+ *      - flag : type Flags : the flag we are marking as on in the table
+ *
+ * Returns :
+ *      - if the flag was already in the table, returns False, otherwise it returns True
  */
 boolean FlagLookupTable_put(FlagLookupTable *flagtable, Flags flag);
 
