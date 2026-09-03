a51c3e7b154b5cade642167eb3d4a1f9f6b9cf21
Emmett-Kopczynski <emmettkopo@gmail.com>
2026-09-02 23:22:42 -0700

    made skeleton code for the codgen_util.c and codegen_util.h files

--- compiler/codegen/codegen_utils.c
+++ compiler/codegen/codegen_utils.c
@@ -11,8 +11,39 @@
 
 
 /* c inclusions */
+#include <stdio.h>
 
 
 /* homemade inclusions */
 #include "codegen_utils.h"
+
+
+void assembly_ast_printer(Assembly_AST *assembly_ast){
+    return;
+} /* TODO implement */
+
+
+Assembly_AST *gen_program(Assembly_AST *assembly_ast, AST *ast){
+    return NULL;
+} /* TODO implement */
+
+
+Assembly_AST *gen_function(AST *ast){
+    return NULL;
+} /* TODO implement */
+
+
+Assembly_AST **gen_instructions(AST *ast){
+    return NULL;
+} /* TODO implement */
+
+
+Assembly_AST *gen_operand(AST *ast){
+    return NULL;
+} /* TODO implement */
+
+
+int free_assembly_ast(Assembly_AST *assembly_ast){
+    return -1;
+} /* TODO implement */
 
--- compiler/codegen/codegen_utils.h
+++ compiler/codegen/codegen_utils.h
@@ -6,6 +6,12 @@
  *      
  *      To Document :
  *          - codegen_utils.h
+ *          - assembly_ast_printer
+ *          - gen_program
+ *          - gen_function
+ *          - gen_instructions
+ *          - gen_operand
+ *          - free_assembly_ast
  *
  */
 
@@ -13,6 +19,44 @@
 /* c inclusions */
 
 /* homemade inclusions */
+#include "assembly_ast.h"
+#include "../parser/ast.h"
+#include "../../util/boolean.h"
+
+
+/* TODO document
+ *
+ */
+void assembly_ast_printer(Assembly_AST *assembly_ast);
+
+
+/* TODO document
+ *
+ */
+Assembly_AST *gen_program(Assembly_AST *assembly_ast, AST *ast);
+
+
+/* TODO document
+ *
+ */
+Assembly_AST *gen_function(AST *ast);
+
+
+/* TODO document
+ *
+ */
+Assembly_AST **gen_instructions(AST *ast);
+
+/* TODO document 
+ *
+ */
+Assembly_AST *gen_operand(AST *ast);
+
+
+/* TODO document 
+ *
+ */
+int free_assembly_ast(Assembly_AST *assembly_ast);
 
 
 #endif
