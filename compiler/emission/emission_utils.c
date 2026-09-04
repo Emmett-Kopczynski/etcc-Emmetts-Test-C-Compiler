ef2f63f9d08c915cf6618e0ea822ec3146e4baa7
Emmett-Kopczynski <emmettkopo@gmail.com>
2026-09-04 08:43:16 -0700

    implemented all functions in compiler/emission/emission_utils.c, requires testing

--- compiler/emission/emission_utils.c
+++ compiler/emission/emission_utils.c
@@ -6,10 +6,12 @@
  *          - emmision_utils.c
  *
  *      To Implement :
+ *          - emit_instruction
+ *      
+ *      To Test :
  *          - emit_program
  *          - emit_function
- *          - emit_instruction
- *
+ *          
  */
 
 /* c inclusions */
@@ -19,16 +21,66 @@
 
 
 int emit_program(FILE *assembly_source, Assembly_AST *ass_ast){
-    return -1;
-} /* TODO implement */
+    if(ass_ast->node_type != A_PROGRAM) goto error;
+    
+    fprintf(assembly_source, "\n\n");
+    int err; err = 0;
+    err = emit_function(assembly_source, ass_ast->node.aprog->type.afunc.afunc); 
+    if(err != 0) goto error;
+    
+    fprintf(assembly_source, "\n\n");
+    fprintf(assembly_source, "#this line indicates that we don't need an executable stack\n");
+    fprintf(assembly_source, ".section .note.GNU-stack,\"\",@progbits\n"); 
+    return 0;
+error:
+    fprintf(stderr, "ERROR EMITING PROGRAM\n");
+    return 1;
+} /* TODO test */
 
 
 int emit_function(FILE *assembly_source, Assembly_AST *ass_ast){
-    return -1;
-} /* TODO implement */
+    if(ass_ast->node_type != A_FUNCTION) goto error;
+
+    fprintf(assembly_source, "\t.globl %s\n", ass_ast->node.afunc->type.a_tempdef.identifier->info); 
+    fprintf(assembly_source, "%s:\n", ass_ast->node.afunc->type.a_tempdef.identifier->info);
+
+    int i; i = 0;
+    int err; err = 0;
+    for(i = 0; i < INSTCOUNT; i++){
+        err = emit_instruction(assembly_source, ass_ast->node.afunc->type.a_tempdef.a_inst[i]);
+        if(err != 0) goto error;
+    }
+
+    return 0;
+error:
+    fprintf(stderr, "ERROR EMITING FUNCTION\n");
+    return 1;
+} /* TODO test */
 
 
 int emit_instruction(FILE *assembly_source, Assembly_AST *ass_ast){
-    return -1;
+    if(ass_ast->node_type != A_INSTRUCTION) goto error;  
+    
+    switch(ass_ast->node.ainst->Instruct_Type){
+        case MOV:
+            A_Operand *op = ass_ast->node.ainst->type.a_mov.exp_op->node.aoper;
+
+            /* NOTE register %eax is hardcoded here, in future versions this will NOT be the case */
+            fprintf(assembly_source, "\tmovl %s, %%eax\n", op->type.conint.con->info);
+            break;
+
+        case RET:
+            fprintf(assembly_source, "\tret\n");
+            break;
+
+        default:
+            goto error;
+    }
+    return 0;
+error:
+    fprintf(stderr, "ERROR EMITING INSTRUCTION\n");
+    return 1;
 } /* TODO implement */
+
+
 
