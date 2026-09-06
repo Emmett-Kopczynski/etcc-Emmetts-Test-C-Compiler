9a1a5cc34cffaa93e0b1788bc40050eb7bdc64a0
Emmett-Kopczynski <emmettkopo@gmail.com>
2026-09-05 17:49:25 -0700

    mild update to the boolean.h documentation

--- util/boolean.h
+++ util/boolean.h
@@ -1,9 +1,17 @@
 #ifndef BOOLEAN_H_
 #define BOOLEAN_H_
+/* boolean.h : TODO document 
+ *
+ */
 
+
+/* boolean : TODO document
+ *
+ */
 typedef enum{
     False, 
     True
 } boolean;
+
 
 #endif
